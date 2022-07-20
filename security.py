6707b44d1c568720ee48c9d0e5850c3775aa3a3d
austinfuentes1313 <100821630+austinfuentes1313@users.noreply.github.com>
2022-07-19 19:02:58 -0500

    Fixed not importing db when running the app.2

--- security.py
+++ security.py
@@ -1,10 +1,10 @@
-from hmac import compare_digest
+from werkzeug.security import safe_str_cmp
 from models.user import UserModel
 
 
 def authenticate(username, password):
     user = UserModel.find_by_username(username)
-    if user and compare_digest(user.password, password):
+    if user and safe_str_cmp(user.password, password):
         return user
 
 
